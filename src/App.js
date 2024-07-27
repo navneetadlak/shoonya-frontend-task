05a498f2f0732f5e2e961446304bf5c566287acf
Navneet Adlak <aniketadlak9@gmail.com>
2024-07-27 15:06:27 +0530

    use effect implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Homepage from "./Component/Homepage";
+import Navbar from "./Component/Navbar";
 
 function App() {
+  const [retreats, setRetreats] = useState([]);
+  const [search, setSearch] = useState("");
+  const [filter, setFilter] = useState({ type: "", date: "" });
+  const [totalPages, setTotalPages] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [filteredRetreats, setFilteredRetreats] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      `https://669f704cb132e2c136fdd9a0.mockapi.io/api/v1/retreats?page=${currentPage}&limit=5`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setRetreats(data);
+        setFilteredRetreats(data);
+      });
+  }, [currentPage]);
+
+  useEffect(() => {
+    let filtered = retreats;
+
+    if (filter.type) {
+      filtered = filtered.filter((retreat) => retreat.type === filter.type);
+    }
+    if (filter.date) {
+      filtered = filtered.filter((retreat) => new Date(retreat.date).toISOString().split('T')[0] === filter.date);
+    }
+    if (searchTerm) {
+      filtered = filtered.filter((retreat) => retreat.title.toLowerCase().includes(searchTerm.toLowerCase()));
+    }
+
+    setFilteredRetreats(filtered);
+  }, [filter, searchTerm, retreats]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Homepage />
     </div>
   );
 }
---
+++ src/Component/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='bg-blue-950 p-4 h-15'>
+    <div className='container mx-auto flex  justify-start font-medium text-slate-50 ml-7'>Wellness Retreats</div>
+
+
+    </nav>
+  
+   
+  )
+}
+
+export default Navbar
